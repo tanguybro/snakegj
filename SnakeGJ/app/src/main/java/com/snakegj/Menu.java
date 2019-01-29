545f12ab5a159317ba54571ca77d93f9539c5bc7
tanguybro <tanguy.brouassin@gmail.com>
2019-01-29 22:48:18 +0100

    debut

---
+++ SnakeGJ/app/src/main/java/com/snakegj/Menu.java
@@ -0,0 +1,18 @@
+package com.snakegj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+       /* TextView text = new TextView(this);
+        text.setText("Bonjour, voici le futur meilleur jeu du monde.");
+        setContentView(text);*/
+    }
+}
