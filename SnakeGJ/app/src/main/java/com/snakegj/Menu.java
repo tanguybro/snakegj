6e9a73100d0fa0c9b8506b17a1768082b0c733fd
Axel.Razafimahefa <axel.raza75@hotmail.fr>
2019-01-31 16:59:52 +0100

    Ajout bouton et nouvelles activitees

--- SnakeGJ/app/src/main/java/com/snakegj/Menu.java
+++ SnakeGJ/app/src/main/java/com/snakegj/Menu.java
@@ -1,18 +1,47 @@
 package com.snakegj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
+
+    private Button btnJouer;
+    private Button btnScores;
+    private Button btnOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
-       /* TextView text = new TextView(this);
-        text.setText("Bonjour, voici le futur meilleur jeu du monde.");
-        setContentView(text);*/
+        btnJouer = (Button) findViewById(R.id.jouer);
+        btnScores = (Button) findViewById(R.id.scores);
+        btnOptions = (Button) findViewById(R.id.options);
+
+        btnJouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Jeu.class));
+            }
+        });
+
+        btnScores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Jeu.class));
+            }
+        });
+
+        btnOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Jeu.class));
+            }
+        });
+
     }
 }
