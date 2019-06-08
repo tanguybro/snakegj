c8bd559ebcbc9af29aa3674b442ce9000df32e74
Tanguy <tanguy.brouassin@gmail.com>
2019-06-08 18:19:05 +0200

    yes

---
+++ SnakeGJ/app/src/main/java/com/snakegj/popup/PopupTutoriel.java
@@ -0,0 +1,29 @@
+package com.snakegj.popup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.snakegj.R;
+import com.snakegj.jeu.Jeu;
+
+public class PopupTutoriel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup_tutoriel);
+
+        Button btnOK= findViewById(R.id.btnOk);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PopupTutoriel.this, Jeu.class));
+                finish();
+            }
+        });
+    }
+
+}
