6d4fdc7a51cb5bef20a21e2d6707839addf4dfbc
Axel.Razafimahefa <axel.raza75@hotmail.fr>
2019-10-27 21:06:00 +0100

    fix fruit position

--- SnakeGJ/app/src/main/java/com/snakegj/jeu/snake/elementsGraphiques/Fruit.java
+++ SnakeGJ/app/src/main/java/com/snakegj/jeu/snake/elementsGraphiques/Fruit.java
@@ -1,14 +1,15 @@
 package com.snakegj.jeu.snake.elementsGraphiques;
 
 import android.graphics.Canvas;
+import android.util.Log;
 
 public class Fruit extends ElementGraphique {
     private int x, y;
     private int hauteur, largeur;
 
     public Fruit() {
-        x = 100;
-        y = 100;
+        x = 270;
+        y = 345;
     }
 
     public int getX() {
@@ -29,10 +30,34 @@ public class Fruit extends ElementGraphique {
 
     public void apparaitre(Serpent serpent) {
         do {
-            x = (int) (Math.random() * (FondJeu.getLargeur() - (largeur/2)));
-            y = (int) (Math.random() * (FondJeu.getHauteur() - (hauteur/2)));
+            int uniteX = FondJeu.getLargeur() / Serpent.getLargeurAnneau();
+            int uniteY = FondJeu.getHauteur() / Serpent.getHauteurAnneau();
+            int randomX = randomNumber(0, uniteX);
+            int randomY = randomNumber(0, uniteY);
+            int xtemp = randomX * Serpent.getLargeurAnneau();
+            int ytemp = randomY * Serpent.getHauteurAnneau();
+
+            if(xtemp > FondJeu.getLargeur() - Serpent.getLargeurAnneau())
+                x = FondJeu.getLargeur() - Serpent.getLargeurAnneau();
+            else
+                x = xtemp;
+
+            if(ytemp > FondJeu.getHauteur() - Serpent.getHauteurAnneau())
+                y = FondJeu.getHauteur() - Serpent.getHauteurAnneau();
+            else
+                y = ytemp;
+
+            Log.d("positionX", x + "");
+            Log.d("positionY", y + "");
+            Log.d("positionFX", FondJeu.getLargeur() + "");
+            Log.d("positionFY", FondJeu.getHauteur() + "");
+
         }
         while(serpent.posDansSerpent(x,y));
+    }
+
+    private int randomNumber(int min, int max) {
+        return min + (int)(Math.random() * ((max - min) + 1));
     }
 
     public void dessiner(Canvas canvas) {
